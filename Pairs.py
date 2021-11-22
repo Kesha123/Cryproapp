826c620d3f745cc1d8adaabde7734f37bd5656e4
Kesha123 <bopuclav42@gmail.com>
2021-11-22 16:26:08 +0200

    Connection is fixed

--- Pairs.py
+++ Pairs.py
@@ -5,6 +5,10 @@ import requests
 import datetime
 import certifi
 import os
+#import nest_asyncio
+import time as Time
+
+#nest_asyncio.apply()
 
 os.environ['SSL_CERT_FILE'] = certifi.where()
 connectionID = 12345
@@ -17,6 +21,7 @@ def create_user():
     socket = "{}?token={}&connectId={}".format(endpoint, token, connectionID)
     ping_interval = url.get('data').get('instanceServers')[0].get('pingInterval')
     ping_timeout = url.get('data').get('instanceServers')[0].get('pingTimeout')
+    print(ping_interval, ping_timeout)
     return socket, ping_interval, ping_timeout
 
 
@@ -33,11 +38,14 @@ class Pair(object):
         self.data = []
         self.xdata = []
         self.ydata = []
-        self.full_time = ''
+        self.timer = 0
 
     async def main(self, websocket=create_user()):
         async with websockets.connect(websocket[0], ping_interval=websocket[1],
                                       ping_timeout=websocket[2]) as connection:
+
+            pingInterval = websocket[1]//1000
+            pingTimeout = websocket[2]//1000
 
             data = json.dumps({
                 "id": f"{connectionID}",
@@ -52,20 +60,42 @@ class Pair(object):
             result = await connection.recv()
 
             while True:
-                data = json.loads(await connection.recv()).get('data')
+                start_time = Time.time()
+                response = json.loads(await connection.recv())
+                try:
+                    data = response.get('data')
 
-                time = datetime.datetime.fromtimestamp(int(data.get('time')) // 1000).strftime("%S")
-                full_time = datetime.datetime.fromtimestamp(int(data.get('time')) // 1000).strftime("%d.%m.%Y %H:%M")
-                price = data.get('price')
+                    time = datetime.datetime.fromtimestamp(int(data.get('time')) // 1000).strftime("%S")
+                    full_time = datetime.datetime.fromtimestamp(int(data.get('time')) // 1000).strftime("%d.%m.%Y %H:%M")
+                    price = data.get('price')
 
-                if (time not in self.xdata) and (price not in self.ydata):
-                    coin_data = (float(time), float(price), full_time, self.name,)
-                    self.data.append(coin_data)
-                    self.xdata.append(time)
-                    self.ydata.append(float(price))
+                    if (time not in self.xdata) and (price not in self.ydata):
+                        coin_data = (float(time), float(price), full_time, self.name,)
+                        self.data.append(coin_data)
+                        self.xdata.append(time)
+                        self.ydata.append(float(price))
 
-                    if float(time) == 59.0:
-                        self.data.clear()
+                        if float(time) == 59.0:
+                            self.xdata.clear()
+                            self.ydata.clear()
+                            self.data.clear()
+
+                    self.timer += Time.time() - start_time
+
+                    if pingTimeout <= float(self.timer) <= pingInterval:
+                        ping = json.dumps(
+                            {
+                                "id": connectionID,
+                                "type": "ping",
+                            }
+                        )
+                        await connection.send(ping)
+                        self.timer *= 0
+
+                except Exception as ex:
+                    print(ex)
+
+                print(self.ydata)
 
     def __delete__(self, instance):
         print("Pair is deleted")
@@ -82,4 +112,6 @@ def start(pair):
 
 
 if __name__ == '__main__':
+    #p = Pair("BTC-USDT")
+    #start(p)
     pass
